44ed1101e6d592f83eda054a9b25f4838110aadf
alexis <alexissimoes94@gmail.com>
2023-08-28 09:03:53 +0200

    v3

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const Discord = require('discord.js');
+const fs = require('fs');
 const { Intents } = require('discord.js'); // Ajout de l'import pour les intents
 const client = new Discord.Client({
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] // Ajoutez d'autres intents si nécessaire
 });
-const token = 'MTE0NDAxNTIzMzMxNzMzOTIzNw.GgJ0dX.R0TetNDlrRU_I652uF9UkQjwN_EreRGz9UQxLc'; // Remplacez ceci par votre propre token de bot
+const token = fs.readFileSync('token.txt', 'utf8').trim();
+// const token = 'MTE0NDAxNTIzMzMxNzMzOTIzNw.GgJ0dX.R0TetNDlrRU_I652uF9UkQjwN_EreRGz9UQxLc'; // Remplacez ceci par votre propre token de bot
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
